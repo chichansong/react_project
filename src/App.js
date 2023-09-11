3b533211f4dba65c312757c4a259f52f3249ef32
chomune <chichansong@gmail.com>
2023-09-11 08:24:00 +0000

    Update App.js and yarn.lock

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>CCS TEST!!</h2>
+      <h2>CCS TEST!! 20230911!!!!</h2>
     </div>
   );
 }
