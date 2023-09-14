05be049e333cad4806b34ced32657027483ad799
chichansong <chichansong@gmail.com>
2023-09-14 04:24:53 +0000

    msg

--- server/server.js
+++ server/server.js
@@ -22,3 +22,60 @@ const j = schedule.scheduleJob('10 * * * * *', function() {
 //	console.log("매 10초마다 실행");
 });
 
+
+
+
+/* h2 db test */
+/*
+const JDBC = require('jdbc');
+const jinst = require('jdbc/lib/jinst');
+
+if (!jinst.isJvmCreated()) {
+  jinst.addOption("-Xrs");
+  jinst.setupClasspath(['../h2-1.4.200.jar']);
+}
+
+let h2 = new JDBC({
+  url: 'jdbc:h2:~/test',
+  drivername: 'org.h2.Driver',
+  properties: {
+    user : '',
+    password: ''
+  }
+});
+
+let h2Init = false;
+
+function getH2(callback) {
+  if (!h2Init)
+    h2.initialize((err) => {
+      h2Init = true;
+      callback(err);
+    });
+  return callback(null);
+};
+
+function queryDB(sql, callback) {
+  h2.reserve((err, connobj) => {
+    connobj.conn.createStatement((err, statement) => {
+      if (callback) {
+        statement.executeQuery(sql, (err, result) => h2.release(connobj, (err) => callback(result)));
+      } else {
+        statement.executeUpdate(sql, (err) => h2.release(connobj, (err) => { if (err) console.log(err) }));
+      }
+    });
+  });
+};
+
+module.exports = {
+  initialize: function(callback) {
+    getH2((err) => {
+      queryDB("SELECT * FROM TB_LTWINNUM", (result)=> {
+       result.toObjArray((err, results) => {
+	        console.log("h2 db test");
+		console.log(results);
+       });
+      });
+    });
+ 
+},*/
