b73438843d52349bc47d38771c1116f652f212ba
chomune <chichansong@gmail.com>
2023-11-09 08:02:42 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,91 @@
 import "./styles.css";
 
+import { Route, Link } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+import Divider from "@mui/material/Divider";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>CCS TEST!! 20230911!!!!</h2>
+      <Stack spacing={2} sx={{ flexGrow: 1 }}>
+        <ThemeProvider theme={darkTheme}>
+          <AppBar position="static" color="primary" enableColorOnDark>
+            <Toolbar>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1 }}
+              >
+                {"REACT PROJECT"}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </ThemeProvider>
+      </Stack>
+
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+        }}
+      >
+        {/* dev 1 start */}
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <ImageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Lotto analysis" secondary="Nov 2023" />
+        </ListItem>
+
+        {/* dev 1 end */}
+        <Divider variant="inset" component="li" />
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <WorkIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="TEST" secondary="Jan 9999" />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <BeachAccessIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="TEST" secondary="Jan 9999" />
+        </ListItem>
+      </List>
     </div>
   );
 }
