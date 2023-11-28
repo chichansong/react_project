f8d8d40fd6e0b89f1dc4aef6bf786433b40f866e
chomune <chichansong@gmail.com>
2023-11-28 15:09:25 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,20 +6,20 @@ const port = 5000;
 app.listen(port, () => console.log(`${port}`));
 
 const http = require("http");
-const options = {
-  host: "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903",
-  port: 80,
-  path: "/",
-};
 
 //서버 호출시 로또 당첨번호 API 가져오기
 //파라미터 숫자 당첨회차
 app.get("/", (req, res) => {
-  //res.send("Hellow World!! CSS!!\n");
-
-  http.get(options, function (httpRes) {
-    httpRes.on("data", function (result) {
-      res.send(result);
-    });
-  });
+  http.get(
+    {
+      host: "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903",
+      port: 80,
+      path: "/",
+    },
+    function (httpRes) {
+      httpRes.on("data", function (result) {
+        res.send(result);
+      });
+    },
+  );
 });
